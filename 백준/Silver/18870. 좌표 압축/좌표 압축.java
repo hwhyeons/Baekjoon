6834c03296e0129fb0449f39f27a10fd2e71377b
AndroidStd <86088505+AndroidStd@users.noreply.github.com>
2022-12-23 21:51:11 +0900

    [Silver II] Title: 좌표 압축, Time: 2452 ms, Memory: 364016 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -8,8 +8,8 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         final int COUNT = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        HashMap<Integer,Integer> map = (HashMap<Integer,Integer>)Arrays.stream(arr).boxed().collect(Collectors.toSet()).stream().sorted().collect(Collectors.toMap(Integer::intValue,(val)->sum++));
+        Integer[] arr = Arrays.stream(br.readLine().split(" ")).map(Integer::parseInt).toArray(Integer[]::new);
+        HashMap<Integer,Integer> map = (HashMap<Integer,Integer>)Arrays.stream(arr).distinct().sorted().collect(Collectors.toMap(Integer::intValue,(val)->sum++));
         Arrays.stream(arr).forEach(i-> sb.append(map.get(i)).append(" "));
         System.out.println(sb.substring(0,sb.length()-1));
     }
