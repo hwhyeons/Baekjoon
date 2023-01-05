3f21c2299877460c5f710d4145e3d249b6266bf9
AndroidStd <86088505+AndroidStd@users.noreply.github.com>
2023-01-06 00:17:04 +0900

    [Silver I] Title: 히오스 프로게이머, Time: 240 ms, Memory: 20936 KB -BaekjoonHub

--- 백준/Silver/16564. 히오스 프로게이머/히오스 프로게이머.java
+++ 백준/Silver/16564. 히오스 프로게이머/히오스 프로게이머.java
@@ -1,37 +1,38 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.Collectors;
 
-public class Main {
+public class Main{
     public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("test.txt")));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int remain = Integer.parseInt(br.readLine().split(" ")[1]);
-        TreeMap<Integer,Long> map = new TreeMap<>();
-        map.putAll(br.lines().map(Integer::parseInt).collect(Collectors.groupingBy(i->i,Collectors.counting())));
-        Map.Entry<Integer,Long> minMap,nextMinMap;
-        while (true) {
-            minMap = map.pollFirstEntry();
-            int multiplyConst = minMap.getValue().intValue(); // long 타입 -> int 타입
-            int cur = minMap.getKey();
-            if (map.isEmpty()) {
-                int plusCount = remain/multiplyConst;
-                System.out.println(cur+plusCount);
-                return;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] levels = new int[n];
+        long max = Integer.MAX_VALUE;
+        long min = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++){
+            levels[i] = Integer.parseInt(br.readLine());
+            min = Math.min(min, levels[i]);
+        }
+        long answer = 0;
+        while (min <= max) {
+            long mid = (max + min) / 2;
+            long sum = 0;
+            for (int i = 0; i < n; i++) {
+                if (mid >= levels[i]) {
+                    sum += mid - levels[i];
+                }
             }
-            nextMinMap = map.firstEntry();
-            int next = nextMinMap.getKey();
-            int gap = next-cur;
-            int bestPlusCount = gap;
-            int plusCount = remain/multiplyConst; // 최대 줄 수 있는 개수
-            if (plusCount >= bestPlusCount) {
-                // 맵 원소 변경
-                map.put(next,multiplyConst+nextMinMap.getValue());
-                remain-=bestPlusCount*multiplyConst;
-                continue;
-            } else { // 못주는 경우
-                System.out.println(cur+plusCount);
-                return;
+            if (k >= sum) {
+                min = mid + 1;
+                answer = Math.max(answer, mid);
+            } else {
+                max = mid - 1;
             }
         }
+        System.out.println(answer);
     }
 }
+
+
