67aa6071c3caa01dee10a6fb219394c4ec4eb679
AndroidStd <86088505+AndroidStd@users.noreply.github.com>
2023-01-09 16:32:50 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <sstream>
+#include <stdio.h>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	int a,b; // 테스트 케이스 갯 
+	scanf("%d %d",&a,&b);
+	printf("%d",a+b);
+	return 0;
+}
+
+//%d
+
+
