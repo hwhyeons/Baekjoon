2a0ab925b6447f848db0cde0e95fa68c8fc7522b
AndroidStd <86088505+AndroidStd@users.noreply.github.com>
2023-01-13 02:20:55 +0900

    [Silver V] Title: 다리 놓기, Time: 76 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -0,0 +1,13 @@
+
+def f(n):
+    ans = 1
+    for i in range(1,n+1):
+        ans *= i
+    return ans
+
+t = int(input())
+for i in range(t):
+    s = input().split(" ")
+    r = int(s[0])
+    n = int(s[1])
+    print(f(n)//(f(r)*f(n-r)))
