45501930cb582fe8dff99578de08f3bde660c0b6
AndroidStd <86088505+AndroidStd@users.noreply.github.com>
2023-01-17 01:29:30 +0900

    [Bronze II] Title: 단어의 개수, Time: 524 ms, Memory: 37988 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -2,7 +2,13 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
+/*
+        Scanner.tokens()로 스트림 사용
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.tokens().count());
+ */
 /*
 //        베열 + split 테스트
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,6 +23,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        System.out.println(sc.tokens().count());
+        String[] arr = sc.nextLine().split(" ");
+        int minus = 0;
+        if (arr.length !=0) {
+            if (arr[0].equals("")) minus++;
+            if (arr.length >=2&& arr[1].equals("")) minus++;
+        }
+        System.out.println(arr.length-minus);
     }
 }
