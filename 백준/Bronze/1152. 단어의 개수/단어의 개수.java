9b363405d8e639e6cc6852a7fec628af5f71df76
AndroidStd <86088505+AndroidStd@users.noreply.github.com>
2023-01-17 01:29:54 +0900

    [Bronze II] Title: 단어의 개수, Time: 288 ms, Memory: 26416 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,29 +1,10 @@
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Scanner;
-import java.util.StringTokenizer;
 
-/*
-        Scanner.tokens()로 스트림 사용
-        Scanner sc = new Scanner(System.in);
-        System.out.println(sc.tokens().count());
- */
-/*
-//        베열 + split 테스트
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] arr = br.readLine().split(" ");
-        int minus = 0;
-        if (arr.length !=0) {
-            if (arr[0].equals("")) minus++;
-            if (arr.length >=2&& arr[1].equals("")) minus++;
-        }
-        System.out.println(arr.length-minus);
- */
 public class Main {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        String[] arr = sc.nextLine().split(" ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] arr = br.readLine().split(" ");
         int minus = 0;
         if (arr.length !=0) {
             if (arr[0].equals("")) minus++;
