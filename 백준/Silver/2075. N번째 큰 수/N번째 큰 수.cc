cd7cfcc48df02226d37ca3f3b8ef2c8451efd04e
AndroidStd <86088505+AndroidStd@users.noreply.github.com>
2023-01-20 16:39:47 +0900

    [Silver II] Title: N번째 큰 수, Time: 284 ms, Memory: 10916 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
@@ -4,28 +4,37 @@
 #include <iostream>
 #include <stack>
 #include <queue>
-
+#include <climits>
 
 using namespace std;
 
 
 int main() {
 	ios::sync_with_stdio(false); cin.tie(NULL);
-	int n,tmp;
-	cin >> n;
-	priority_queue<int,vector<int>,greater<int>> pq;
-	int size = n * n;
-	for (int i = 0;i < size; i++) {
-		cin >> tmp;
-		if (pq.size() == n) {
-			if (tmp > pq.top()) {
-				pq.pop();
-				pq.push(tmp);
-			}
-		}
-		else {
-			pq.push(tmp);
-		}
-	}
-	cout << pq.top();
+    int N,tmp;
+    cin >> N;
+    int** arr = new int*[N];
+    int* lineIndex = new int[N];
+    fill_n(lineIndex, N, N - 1);
+    for (int i = 0; i < N; i++) {
+        arr[i] = new int[N];
+        for (int j = 0; j < N; j++) {
+            cin >> tmp;
+            arr[i][j] = tmp;
+        }
+    }
+
+    int max = 0;
+    for (int i = 1; i <= N; i++) {
+        max = INT_MIN;
+        int maxIndex = -1;
+        for (int j = 0; j < N; j++) {
+            if (lineIndex[j] >= 0 && max < arr[lineIndex[j]][j]) {
+                max = arr[lineIndex[j]][j];
+                maxIndex = j;
+            }
+        }
+        lineIndex[maxIndex] -= 1;
+    }
+    cout << max;
 }
