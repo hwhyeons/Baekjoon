24baf2c152f557d93b681633578eb65c24eb7b2e
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2023-01-27 15:02:13 +0900

    [Bronze II] Title: 분산처리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.cc
+++ 백준/Bronze/1009. 분산처리/분산처리.cc
@@ -13,16 +13,28 @@
 
 using namespace std;
 
+// 재귀 함수
+// 나누는 값은 무조건 10
+int f(int a, int n) {
+	if (n == 1) {
+		return a % 10;
+	}
+
+	if (n % 2 == 0) { // 짝수
+		return (((f(a, n / 2)) % 10) * ((f(a, n / 2)) % 10)) % 10;
+	}
+	else { // 홀 수
+		return (((((f(a, n / 2)) % 10) * ((f(a, n / 2)) % 10)) % 10) * (a % 10)) % 10;
+	}
+}
+
 int main() {
 	ios::sync_with_stdio(false); cin.tie(NULL);
 	int t_count,a,b;
 	cin >> t_count;
 	for (int i = 0; i < t_count; i++) {
 		cin >> a >> b;
-		int ans = 1;
-		for (int j = 1; j <= b; j++) {
-			ans = (ans * a%10) % 10;
-		}
+		int ans = f(a, b);
 		if (ans == 0) ans = 10;
 		cout << ans  << "\n";
 	}
