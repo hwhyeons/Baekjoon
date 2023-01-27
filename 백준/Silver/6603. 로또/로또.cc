8124e7d9b2df9b5bb234e55234ea47c2962379c2
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2023-01-27 22:53:52 +0900

    [Silver II] Title: 로또, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.cc
+++ 백준/Silver/6603. 로또/로또.cc
@@ -7,38 +7,29 @@
 
 using namespace std;
 
-void comb(vector<int>& v,vector<int>& out, int idx, int depth) {
-	if (depth == 6) {
-		auto it = out.begin();
-		while (it != out.end()) {
-			cout << *it << " ";
-			it++;
-		}
-		cout << "\n";
-		return;
-	}
-	for (int i = idx; i < v.size();i++) {
-		out[depth] = v[i];
-		comb(v, out, i + 1, depth + 1);
-	}
-}
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
+	
 	int n, t;
 	while (true) {
 		cin >> n;
 		if (n == 0) break;
 		vector<int> v(n);
-		vector<int> out(6);
 		for (int i = 0;i < n;i++) {
 			cin >> t;
 			v[i] = t;
 		}
+		vector<int> idxs(n);
 		sort(v.begin(), v.begin() + n);
-		comb(v, out,0,0);
-		cout << "\n";
+		fill(idxs.begin(), idxs.begin() + 6, 1);
+		do {
+			for (int i = 0; i < idxs.size(); i++) {
+				if (idxs[i] == 1) cout << v[i] << " ";
+			}
+			cout << "\n";
+		} while (prev_permutation(idxs.begin(),idxs.end()));
+		cout << "\n";	
 	}
 }
-
