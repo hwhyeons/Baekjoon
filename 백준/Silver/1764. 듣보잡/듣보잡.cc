a821dc990a6dbf13c372943c8ca2f62b1d4df22c
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2023-02-15 13:39:54 +0900

    [Silver IV] Title: 듣보잡, Time: 44 ms, Memory: 7580 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <numeric>
+#include <deque>
+#include <set>
+
+using namespace std;
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n,k;
+	string t;
+	cin >> n >> k;
+	set<string> s;
+	for (int i =0;i<n;i++) {
+		cin >> t;
+		s.insert(t);
+	}
+	vector<string> ans;
+	int cnt = 0;
+	for (int i =0;i<k;i++) {
+		cin >> t;
+		if (s.find(t) == s.end()) { // ok
+			continue;
+		} else {
+			cnt++;
+			ans.push_back(t);
+		}
+	}
+
+	sort(ans.begin(),ans.end());
+	cout << cnt << endl;
+	for (auto i = ans.begin(); i != ans.end(); i++) {
+		cout << *i << "\n";
+	}
+}
+
+
