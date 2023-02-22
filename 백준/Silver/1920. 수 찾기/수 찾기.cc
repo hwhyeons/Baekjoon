ce2682f8a2cb21543e81f1b33fa298b73df80334
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2023-02-22 21:48:27 +0900

    [Silver IV] Title: 수 찾기, Time: 48 ms, Memory: 2920 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,55 +1,38 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
-#include <cstring>
-#include <sstream>
-#include <vector>
 #include <numeric>
-#include <deque>
-#include <stack>
-#include <unordered_set>
+#include <vector>
+
 
 using namespace std;
-
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
-	
-	int a,b,t;
+	int n;
+	cin >> n;
 	vector<int> v;
-	cin >> a;
-	for (int i =0; i<a; i++) {
+	for (int i =0; i<n; i++) {
+		int t;
 		cin >> t;
 		v.push_back(t);
 	}
-	sort(v.begin(),v.end());
-	
 
-	
-	cin >> b;
-	for (int i =0; i<b; i++) {
+	sort(v.begin(),v.end());
+
+	int m;
+	cin >> m; 
+	for (int i =0; i<m; i++) {
+		int t;
 		cin >> t;
-		int s =0,e=v.size()-1,m;
-		bool ok = false;
-		while (s<=e) {
-			m = (s+e)/2;
-			if (v[m] == t) {
-				ok = true;
-				break;
-			}
-			if (v[m] < t) {
-				s = m+1;
-			} else {
-				e = m - 1;
-			}
-		}
-		if (ok) {
+		if (binary_search(v.begin(),v.end(),t)) {
 			cout << "1\n";
-		} else {
+		}  else {
 			cout << "0\n";
 		}
 	}
 }
+	
 
 
