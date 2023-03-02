91f5eb66e42bda9345fb373dc78b686f53f134f8
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2023-03-02 19:02:28 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <numeric>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <stack>
+#include <set>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    string s = "|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|";
+    cout << s;
+}
