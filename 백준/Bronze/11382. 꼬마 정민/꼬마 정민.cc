d294d9406101540e0a09c8c5142c95937ce7fbaf
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2023-03-02 16:45:39 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <numeric>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <stack>
+#include <set>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    long long a,b,c;
+    cin >> a >> b >> c;
+    cout << a+b+c;
+}
