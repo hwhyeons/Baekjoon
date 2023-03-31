7044fa6add1ffad065e4d0280734e88d792c083a
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2023-04-01 01:19:20 +0900

    [Silver III] Title: N과 M (7), Time: 488 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include <ranges>
+
+typedef long long ll;
+
+using namespace std;
+
+void dfs(vector<int>& v,vector<int>& out, vector<bool>& visit,int n, int r, int depth, int idx) {
+    if (depth == r) {
+        for (int k: out) {
+            cout << k << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    for (int i = 0; i < n; ++i) {
+//        if (visit[i]) continue;
+//        visit[i] = true;
+        out[depth] = v[i];
+        dfs(v,out,visit,n,r,depth+1,i+1);
+//        visit[i] = false;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, r;
+    cin >> n >> r;
+    vector<int> v(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+    }
+    ranges::sort(v);
+    vector<bool> visit(n);
+    vector<int> out(r);
+    dfs(v,out,visit,n,r,0,0);
+
+}
+
