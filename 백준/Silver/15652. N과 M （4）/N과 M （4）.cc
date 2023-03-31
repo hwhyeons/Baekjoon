e4a3f11aeb04391f977ffe730744f1ad2f76ae7f
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2023-04-01 01:14:20 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <ranges>
+
+typedef long long ll;
+
+using namespace std;
+
+void dfs(vector<int>& out, int n, int r, int depth, int idx) {
+    if (depth == r) {
+        for (int k: out) {
+            cout << k << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    for (int i = idx; i <= n; ++i) {
+        out[depth] = i;
+        dfs(out,n,r,depth+1,i);
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, r;
+    cin >> n >> r;
+//    vector<int> v(n);
+//    iota(v.begin(), v.end(), 1);
+    vector<int> out(r);
+    dfs(out,n,r,0,1);
+
+}
+
