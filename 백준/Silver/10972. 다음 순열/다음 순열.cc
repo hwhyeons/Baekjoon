279d6c3b759dc81338f7cc0af2b798981fa86afb
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2023-04-01 20:50:46 +0900

    [Silver III] Title: 다음 순열, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Silver/10972. 다음 순열/다음 순열.cc
@@ -0,0 +1,25 @@
+
+#include <bits/stdc++.h>
+#include <ranges>
+
+typedef long long ll;
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+    }
+    if (next_permutation(v.begin(),v.end())) {
+        for (auto k : v) {
+            cout << k <<" ";
+        }
+    } else {
+        cout << -1;
+    }
+}
