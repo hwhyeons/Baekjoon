3402dce967c752a2c7bf970ac9902e267d514d72
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2023-04-16 13:26:29 +0900

    [Silver IV] Title: 상근이의 여행, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <ranges>
+
+typedef long long ll;
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tcnt;
+    cin >> tcnt;
+    while (tcnt--) {
+        int N,M;
+        cin >> N >> M;
+        int left_cnt = N;
+        int ans = 0;
+        unordered_set<int> s;
+        for (int i = 0; i < M; ++i) {
+            int t1,t2;
+            cin >> t1 >> t2;
+        }
+        cout << N-1 << "\n";
+    }
+
+
+}
