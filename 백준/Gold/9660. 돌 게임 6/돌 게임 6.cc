f33c074a5cf4d4dad276ecba4d9b3ebcf998aae8
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2023-04-19 13:01:29 +0900

    [Gold V] Title: 돌 게임 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/9660. 돌 게임 6/돌 게임 6.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <ranges>
+
+typedef long long ll;
+
+using namespace std;
+
+int main() {
+    ll n;
+    cin >> n;
+    if (n%7 == 0 || (n%7) == 2) {
+        cout << "CY";
+    } else cout << "SK";
+}
