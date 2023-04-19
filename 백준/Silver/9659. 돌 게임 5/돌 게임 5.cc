0301c5bb91abea714338595186a5b47588048916
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2023-04-19 13:00:47 +0900

    [Silver III] Title: 돌 게임 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9659. 돌 게임 5/돌 게임 5.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <ranges>
+
+typedef long long ll;
+
+using namespace std;
+
+int main() {
+    ll n;
+    cin >> n;
+    if (n%2 == 0) {
+        cout << "CY";
+    } else cout << "SK";
+}
