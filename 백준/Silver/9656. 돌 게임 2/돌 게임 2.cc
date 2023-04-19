ebce424ff35530ccf2df6b6ecc16c0e416206516
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2023-04-19 12:46:21 +0900

    [Silver IV] Title: 돌 게임 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <ranges>
+
+typedef long long ll;
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if (n%2 == 1) {
+        cout << "CY";
+    } else cout << "SK";
+}
