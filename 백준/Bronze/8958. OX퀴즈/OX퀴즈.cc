a2ba2daeba4cfdc16bba81011f414d04475b63e9
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2023-05-08 17:11:48 +0900

    [Bronze II] Title: OX퀴즈, Time: 8 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <ranges>
+
+typedef long long ll;
+using namespace std;
+
+int main() {
+    ranges::copy(views::transform(ranges::istream_view<string>(cin) | views::drop(1),[](auto s) {
+        regex r("X+");
+        auto it = sregex_token_iterator(s.begin(),s.end(),r,-1);
+        auto end = sregex_token_iterator();
+        ll acc = 0ll;
+        while (it != end) {
+            int len = (*it).length();
+            acc += ((len)*(len+1))/2;
+            it++;
+        }
+        return acc;
+    }),ostream_iterator<int>(cout,"\n"));
+}
