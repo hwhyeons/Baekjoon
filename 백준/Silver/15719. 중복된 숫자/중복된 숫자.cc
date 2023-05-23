bfa6e125fae0727a19da1b360656cd509e9e05cb
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2023-05-23 13:01:23 +0900

    [Silver V] Title: 중복된 숫자, Time: 1144 ms, Memory: 3240 KB -BaekjoonHub

--- 백준/Silver/15719. 중복된 숫자/중복된 숫자.cc
+++ 백준/Silver/15719. 중복된 숫자/중복된 숫자.cc
@@ -5,14 +5,12 @@ typedef long long ll;
 
 using namespace std;
 
+bitset<10000000> bs;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    int n;
-    cin >> n;
-    vector<int> v(n);
-    for (int i = 0; i < n; ++i) cin >> v[i];
-    ranges::sort(v);
-    cout << *ranges::adjacent_find(v);
+    ranges::for_each(ranges::istream_view<int>(cin),[](int n){
+        if (!(bs[n]=~bs[n])) cout << n;
+    });
     return 0;
 }
