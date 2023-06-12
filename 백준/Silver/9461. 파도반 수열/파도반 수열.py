da2c741ba4f6a5ef6ebb6afbe3088e80519c4161
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2023-06-12 19:56:31 +0900

    [Silver III] Title: 파도반 수열, Time: 80 ms, Memory: 34084 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -0,0 +1,12 @@
+import functools
+
+@functools.cache
+def f(n):
+    if n <= 3:
+        return 1
+    return f(n - 2) + f(n - 3)
+
+tcnt = int(input())
+for tc in range(1, tcnt + 1):
+    n = int(input())
+    print(f(n))
