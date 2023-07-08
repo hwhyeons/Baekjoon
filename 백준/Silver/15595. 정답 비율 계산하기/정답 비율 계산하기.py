9ab042c34e95bfca36f9ef93d7f59819775b64da
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2023-07-08 12:45:10 +0900

    [Silver III] Title: 정답 비율 계산하기, Time: 200 ms, Memory: 37616 KB -BaekjoonHub

---
+++ 백준/Silver/15595. 정답 비율 계산하기/정답 비율 계산하기.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+import sys
+input = sys.stdin.readline
+print = sys.stdout.write
+n = int(input().rstrip("\n"))
+
+d = defaultdict(lambda :[0,False])
+
+for _ in range(n):
+    check_num, user_id, result, mem, time, lang, code_len = input().split()
+    if user_id == "megalusion":
+        continue
+    if result == "4":
+        d[user_id][1] = True
+    else:
+        if d[user_id][1]: # 한번이라도 맞은 적이 있는 경우
+            continue
+        else:
+            d[user_id][0] += 1
+
+if len(d) == 0:
+    print("0")
+else:
+    only_correct = list(map(lambda lst: lst[0], filter(lambda lst: lst[1] == True, d.values())))
+    if not only_correct:
+        print("0")
+    else:
+        ratio = len(only_correct)/(len(only_correct)+(sum(only_correct)))*100
+        print(str(ratio))
