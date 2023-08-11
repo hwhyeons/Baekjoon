e3cc02c9077a08d4fd58ef4aa088c281bf9eebab
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2023-08-11 10:51:29 +0900

    [Bronze II] Title: 기숙사 바닥, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2858. 기숙사 바닥/기숙사 바닥.py
+++ 백준/Bronze/2858. 기숙사 바닥/기숙사 바닥.py
@@ -1,9 +1,6 @@
 r,b=map(int,input().split())
 acc = r+b
 for w in range(1,acc):
-    if acc%w !=0:
-        continue
-    h = acc//w
-    if (w-2)*(h-2) == b:
+    if acc%w ==0 and (w-2)*((h:=acc//w)-2)==b:
         print(h,w)
         break
