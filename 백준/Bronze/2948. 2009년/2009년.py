45cdebbf7a77766c5c909f85b61aa30d94f27d29
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2023-08-12 11:58:53 +0900

    [Bronze II] Title: 2009년, Time: 68 ms, Memory: 38128 KB -BaekjoonHub

--- 백준/Bronze/2948. 2009년/2009년.py
+++ 백준/Bronze/2948. 2009년/2009년.py
@@ -1,2 +1,1 @@
-import datetime
-print(['Mon','Tues','Wednes','Thurs','Fri','Satur','Sun'][datetime.date(2009,*reversed(list(map(int,input().split())))).weekday()]+"day")
+exec("import datetime;print(['Mon','Tues','Wednes','Thurs','Fri','Satur','Sun'][datetime.date(2009,*reversed(list(map(int,input().split())))).weekday()]+'day')")
