3892d3ee4acf37a93875c41fb30ab82c6c83641b
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2023-08-15 20:36:18 +0900

    [Bronze II] Title: 알파벳 전부 쓰기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/11091. 알파벳 전부 쓰기/알파벳 전부 쓰기.py
+++ 백준/Bronze/11091. 알파벳 전부 쓰기/알파벳 전부 쓰기.py
@@ -1,5 +1,2 @@
-tcnt = int(input())
-all={chr(i) for i in range(97,123)}
-for _ in range(tcnt):
-    s=set(map(lambda x:x.lower(),filter(lambda x:x.isalpha(),input())))
-    print("missing "+"".join(k) if (k:=sorted(all.difference(s))) else "pangram")
+for _ in range(int(input())):
+    print("missing "+"".join(k) if (k:=sorted({*map(chr,range(97,123))}.difference(set(map(lambda x:x.lower(),filter(lambda x:x.isalpha(),input())))))) else "pangram")
