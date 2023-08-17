58ea3b32b8c1caa496bceef40308ad7904a9e312
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2023-08-17 12:08:22 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
@@ -1,4 +1,1 @@
-t=int(input())
-v=[input() for _ in range(t)]
-v=list(map(list,zip(*v)))
-print("".join(map(lambda x: x[0] if len(set(x)) == 1 else "?", v)))
+print("".join(map(lambda x: x[0] if len(set(x)) == 1 else "?", map(list,zip(*[input() for _ in range(int(input()))])))))
