29f9ba5d3afc756c8d9f6c31bd9e295dc44e0a47
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2023-08-18 12:44:51 +0900

    [Bronze I] Title: 무알콜 칵테일, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2896. 무알콜 칵테일/무알콜 칵테일.py
+++ 백준/Bronze/2896. 무알콜 칵테일/무알콜 칵테일.py
@@ -1,4 +1,2 @@
-a,b,c=map(int,input().split())
-l,j,k=map(int,input().split())
-ratio=min(a/l,b/j,c/k)
-print(a-l*ratio,b-j*ratio,c-k*ratio)
+a,b,c,l,j,k=map(int,open(0).read().split())
+print(a-l*(r:=min(a/l,b/j,c/k)),b-j*r,c-k*r)
