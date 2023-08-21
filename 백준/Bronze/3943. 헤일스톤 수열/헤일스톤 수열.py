9b0bf72859692c431cadb57911dd7eca2bdcba77
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2023-08-21 13:13:28 +0900

    [Bronze II] Title: 헤일스톤 수열, Time: 1572 ms, Memory: 149900 KB -BaekjoonHub

--- 백준/Bronze/3943. 헤일스톤 수열/헤일스톤 수열.py
+++ 백준/Bronze/3943. 헤일스톤 수열/헤일스톤 수열.py
@@ -1,8 +1,18 @@
-t = int(input())
-for _ in range(t):
-    n=int(input())
+import sys
+input = sys.stdin.readline
+
+d={}
+def f(n):
+    if n==1:
+        return 1
+    if n in d:
+        return d[n]
     mx = n
-    while n != 1:
-        n = n*3+1 if n%2 else n//2
-        mx=max(mx,n)
-    print(mx)
+    n = n*3+1 if n%2 else n//2
+    mx = max(mx,f(n))
+    return mx
+
+t = int(input().rstrip())
+for _ in range(t):
+    n=int(input().rstrip())
+    print(f(n))
