97659255eabeb854db892c41f8a5b4e7aa6bd14e
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2023-08-22 13:07:49 +0900

    [Silver V] Title: 끝말잇기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/28432. 끝말잇기/끝말잇기.py
+++ 백준/Silver/28432. 끝말잇기/끝말잇기.py
@@ -1,10 +1,8 @@
 v=open(0).read().split()
 n= int(v[0])
-first,last = 0,0
 c={*v[1:n+1]}
-for i in range(1,n+1):
+for i in range(n+1):
     if v[i] == '?':
-        first=v[i-1][-1] if i>1 else 0
-        last=v[i+1][0] if i!=n else 0
-# print(first,last)
-print(*[s for s in v[n+2:] if (s[0]==first or first==0) and (s[-1]==last or last==0) and s not in c])
+        f=v[i-1][-1] if i>1 else 0
+        l=v[i+1][0] if i!=n else 0
+print(*[s for s in v[n+2:] if (s[0]==f or f==0) and (s[-1]==l or l==0) and s not in c])
