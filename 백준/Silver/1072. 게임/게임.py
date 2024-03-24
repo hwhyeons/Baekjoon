5f380b913ce7521ae5295013a29d8b4a50b6a409
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2024-03-24 13:19:44 +0900

    [Silver III] Title: 게임, Time: 44 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.py
+++ 백준/Silver/1072. 게임/게임.py
@@ -7,7 +7,7 @@ if Y==X:
 s=1
 e=20000000000
 
-Z = int((Y*100)/X)
+Z = int(Y/X*100+0.0000000000001)
 if Z >= 99:
     print(-1)
     exit(0)
@@ -15,18 +15,16 @@ if Z >= 99:
 start_z = Z
 
 min_answer = math.inf
-before_shrink = False # 처음에는 무조건 늘림
 while s<=e:
     m = (s+e)//2
     cur_up = Y+m # 분자
     cur_down =X+m # 분모
-    cur_z = int((cur_up*100/cur_down))
+    cur_z = int(cur_up/cur_down*100+0.0000000000001)
     if cur_z == start_z:
         s = m +1
     else:
         e = m -1
         min_answer = min(min_answer, m)
-
 
 print(min_answer)
 
