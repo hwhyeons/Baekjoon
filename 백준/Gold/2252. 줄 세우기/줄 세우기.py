217aa8ee78ec4f4ad9f2f46e2669526beedf9d48
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2024-05-16 15:27:04 +0900

    [Gold III] Title: 줄 세우기, Time: 444 ms, Memory: 134620 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -3,6 +3,5 @@ N,M=map(int,input().split())
 d={k:set() for k in range(1,N+1)}
 for _ in range(M):
     a,b=map(int,input().split())
-    d[b].add(a)
-gp=graphlib.TopologicalSorter(d)
-print(*gp.static_order())
+    d[b]|={a}
+print(*graphlib.TopologicalSorter(d).static_order())
