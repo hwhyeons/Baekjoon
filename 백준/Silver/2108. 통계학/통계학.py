80e1473c992e269a70bf602e83d065de64fa4fce
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2024-05-16 14:31:48 +0900

    [Silver III] Title: 통계학, Time: 552 ms, Memory: 72508 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -1,8 +1,5 @@
 import statistics
-N = int(input())
-v = []
-for i in range(N):
-    v.append(int(input()))
+v = [*map(int,open(0))][1:]
 print(round(statistics.mean(v)))
 print(statistics.median(v))
 print(t[1] if len(t:=sorted(statistics.multimode(v)))> 1 else t[0])
