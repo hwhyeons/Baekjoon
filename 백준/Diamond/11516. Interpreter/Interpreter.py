592fcc8ec1b0bf141c9de93cd544c8553326a798
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2024-05-20 16:03:03 +0900

    [Diamond V] Title: Interpreter, Time: 124 ms, Memory: 34900 KB -BaekjoonHub

--- 백준/Diamond/11516. Interpreter/Interpreter.py
+++ 백준/Diamond/11516. Interpreter/Interpreter.py
@@ -1,17 +1,9 @@
 import re
-to_custom_int = lambda m: f'Int({m.group(0)})'
-def to_python_expression(expression_str):
-    s1 = expression_str.replace("/","//")
-    s2 = s1.replace("&&","&")
-    s3 = s2.replace("||","|")
-    s4 = s3.replace("!","~")
-    s5 = re.sub(r'\b\d+\b', to_custom_int, s4)
-    return s5
-
-class CodeParser:
-    def __init__(self):
-        self.deep = 0
-        self.code_str = """\n
+to_int = lambda m: f'Int({m.group(0)})'
+def to_python_expression(exp):
+    return re.sub(r'\b\d+\b', to_int, exp.replace("/", "//").replace("&&", "&").replace("||", "|").replace("!", "~"))
+deep=0
+org = """\n
 class Int(int):
     def __init__(self,val: int):
         self.val = val
@@ -37,39 +29,61 @@ for ch in 'abcdefghijklmnopqrstuvwxyz':
     exec(f"{ch}=Int(0)")
 """
 
-    def parse_next_line(self, ls):
-        ans = ''
-        new_ind = False
-        psline = False
-        if ls.startswith("print"):
-            ans = f'print({to_python_expression(ls[5:].strip())})'
-        elif ls.startswith("set"):
-            ans = f'{ls[3:].strip()[:ls[3:].strip().find("=")]} = {to_python_expression(ls[3:].strip()[ls[3:].strip().find("=") + 1:])}'
-        elif ls.startswith("if"):
-            new_ind = True
-            psline = True
-            ans = f'if {to_python_expression(ls[2:].strip())}:'
-        elif ls.startswith("else"):
-            cp.deep-=1
-            psline = True
-            new_ind = True
-            ans = 'else:'
-        elif "end" in ls:
-            cp.deep-=1
-            return
-        elif ls.startswith("while"):
-            psline = True
-            new_ind = True
-            ans = f'while {to_python_expression(ls[5:].strip())}:'
-        final_answer = ' '*4*self.deep+ans
-        if new_ind:
-            cp.deep+=1
-        self.code_str+=final_answer+"\n"
-        if psline:
-            self.code_str+=' '*4*self.deep+"pass\n"
+class Int(int):
+    def __init__(self,val: int):
+        self.val = val
+    def __add__(self, o):
+        return Int(self.val+o.val)
+    def __sub__(self, o):
+        return Int(self.val-o.val)
+    def __mul__(self, o):
+        return Int(self.val*o.val)
+    def __invert__(self):
+        return Int(1 if self.val == 0 else 0)
+    def __neg__(self):
+        return Int(int.__neg__(self.val))
+    def __and__(self, o):
+        return Int(0) if (o.val == 0 or self.val == 0) else Int(1)
+    def __or__(self, o):
+        return Int(1) if (o.val != 0 or self.val != 0) else Int(0)
+    def __floordiv__(self, o):
+        return Int(self.val//o.val)
+    def __mod__(self, o):
+        return Int(self.val%o.val)
+
+def parse_line(ls):
+    global deep,code_str
+    ans = ''
+    new_ind = 0
+    psline = 0
+    if ls.startswith("print"):
+        ans = f'print({to_python_expression(ls[5:].strip())})'
+    elif ls.startswith("set"):
+        ans = f'{ls[3:].strip()[:ls[3:].strip().find("=")]} = {to_python_expression(ls[3:].strip()[ls[3:].strip().find("=") + 1:])}'
+    elif ls.startswith("if"):
+        new_ind = 1
+        psline = 1
+        ans = f'if {to_python_expression(ls[2:].strip())}:'
+    elif ls.startswith("else"):
+        deep-=1
+        psline = 1
+        new_ind = 1
+        ans = 'else:'
+    elif "end" in ls:
+        deep-=1
+        return
+    elif ls.startswith("while"):
+        psline = 1
+        new_ind = 1
+        ans = f'while {to_python_expression(ls[5:].strip())}:'
+    final_answer = ' '*4*deep+ans
+    deep+=new_ind
+    code_str+=final_answer+"\n"
+    if psline:
+        code_str+=' '*4*deep+"pass\n"
 
 while n:= int(input()):
-    cp = CodeParser()
+    code_str=org;deep=0
     for _ in range(n):
-        cp.parse_next_line(input().strip())
-    exec(cp.code_str)
+        parse_line(input().strip())
+    exec(code_str)
