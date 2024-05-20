f11cebc201e0e47a33d27be6033cec4c49f1ed17
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2024-05-20 15:43:31 +0900

    [Diamond V] Title: Interpreter, Time: 128 ms, Memory: 34888 KB -BaekjoonHub

--- 백준/Diamond/11516. Interpreter/Interpreter.py
+++ 백준/Diamond/11516. Interpreter/Interpreter.py
@@ -55,40 +55,39 @@ for ch in 'abcdefghijklmnopqrstuvwxyz':
     exec(f"{ch}=Int(0)")
 """
 
-    def parse_next_line(self, line_strip: str) -> None:
-        answer = ''
-        indent_add_new = False
-        remove_indent = False
-        add_pass_line = False
-        if line_strip.startswith("print"):
-            answer = f'print({to_python_expression(line_strip[5:].strip())})'
-        elif line_strip.startswith("set"):
-            answer = f'{line_strip[3:].strip()[:line_strip[3:].strip().find("=")]} = {to_python_expression(line_strip[3:].strip()[line_strip[3:].strip().find("=") + 1:])}'
-        elif line_strip.startswith("if"):
-            indent_add_new = True
-            add_pass_line = True
-            answer = f'if {to_python_expression(line_strip[2:].strip())}:'
-        elif line_strip.startswith("else"):
-            remove_indent = True
-            add_pass_line = True
-            indent_add_new = True
-            answer = 'else:'
-        elif "end" in line_strip:
-            remove_indent = True
-        elif line_strip.startswith("while"):
-            add_pass_line = True
-            indent_add_new = True
-            answer = f'while {to_python_expression(line_strip[5:].strip())}:'
-
-        if remove_indent:
+    def parse_next_line(self, ls: str) -> None:
+        ans = ''
+        new_ind = False
+        r_ind = False
+        psline = False
+        if ls.startswith("print"):
+            ans = f'print({to_python_expression(ls[5:].strip())})'
+        elif ls.startswith("set"):
+            ans = f'{ls[3:].strip()[:ls[3:].strip().find("=")]} = {to_python_expression(ls[3:].strip()[ls[3:].strip().find("=") + 1:])}'
+        elif ls.startswith("if"):
+            new_ind = True
+            psline = True
+            ans = f'if {to_python_expression(ls[2:].strip())}:'
+        elif ls.startswith("else"):
+            r_ind = True
+            psline = True
+            new_ind = True
+            ans = 'else:'
+        elif "end" in ls:
+            r_ind = True
+        elif ls.startswith("while"):
+            psline = True
+            new_ind = True
+            ans = f'while {to_python_expression(ls[5:].strip())}:'
+        if r_ind:
             cp.deep-=1
-        if not answer:
+        if not ans:
             return
-        final_answer = ' '*4*self.deep+answer
-        if indent_add_new: # "다음줄"부터 indent 넣을지
+        final_answer = ' '*4*self.deep+ans
+        if new_ind:
             cp.deep+=1
         self.code_str+=final_answer+"\n"
-        if add_pass_line:
+        if psline:
             self.code_str+=' '*4*self.deep+"pass\n"
 
 while n:= int(input()):
