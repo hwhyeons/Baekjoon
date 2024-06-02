d15bae2bca6afd0d6ae0b088d7f89712744946bc
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2024-06-02 20:17:47 +0900

    [Silver V] Title: 집합, Time: 648 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cc
+++ 백준/Silver/11723. 집합/집합.cc
@@ -4,9 +4,8 @@
 #include <stdio.h>
 #include <numeric>
 #include <vector>
-#include <unordered_map>
-#include <unordered_set>
 #include <set>
+#include<bitset>
 #include <map>
 #include <stack>
 
@@ -20,45 +19,34 @@ int main()
     int n, tmp;
     int i = 0;
     cin >> n;
-    set<int> s;
-    set<int> empty_set;
-    set<int> base = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20 };
-    vector<int> v(20);
-    iota(v.begin(), v.end(), 20);
+    bitset<21> base("000000000000000000000");
     while (i++ < n) {
         string str;
         cin >> str;
         if (str == "check") {
             cin >> tmp;
-            cout << (s.find(tmp) != s.end()) << "\n";
+            cout << base[tmp] << "\n";
         }
         else if (str == "add") {
             cin >> tmp;
-            if (s.find(tmp) == s.end()) {
-                s.insert(tmp);
-            }
+            base[tmp] = base[tmp] | 1;
         }
         else if (str == "remove") {
             cin >> tmp;
-            if (s.find(tmp) != s.end()) {
-                s.erase(tmp);
-            }
+            base[tmp] = 0;
         }
         else if (str == "toggle") {
             cin >> tmp;
-            if (s.find(tmp) != s.end()) {
-                s.erase(tmp);
-            }
-            else {
-                s.insert(tmp);
-            }
+            base[tmp].flip();
         }
         else if (str == "all") {
-            s = base;
+            base.reset();
+            base.flip();
+            //s = base;
             // s = set<int>(v.begin(), v.end());
         }
         else if (str == "empty") {
-            s = empty_set;
+            base.reset();
             // s.clear(); 
         }
     }
