caf58e2841bdb23583abb6f4d9c529e70adcc288
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2024-06-02 20:05:02 +0900

    [Silver V] Title: 집합, Time: 664 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cc
+++ 백준/Silver/11723. 집합/집합.cc
@@ -15,41 +15,50 @@ using namespace std;
 
 int main()
 {
-	ios::sync_with_stdio(false);
+    ios::sync_with_stdio(false);
     cin.tie(0);
-    int n,tmp;
-    int i=0;
+    int n, tmp;
+    int i = 0;
     cin >> n;
     set<int> s;
+    set<int> empty_set;
     set<int> base = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20 };
-    while(i++ < n) {
+    vector<int> v(20);
+    iota(v.begin(), v.end(), 20);
+    while (i++ < n) {
         string str;
         cin >> str;
         if (str == "check") {
             cin >> tmp;
             cout << (s.find(tmp) != s.end()) << "\n";
-        } else if (str == "add"){
+        }
+        else if (str == "add") {
             cin >> tmp;
             if (s.find(tmp) == s.end()) {
                 s.insert(tmp);
             }
-        } else if (str == "remove") {
+        }
+        else if (str == "remove") {
             cin >> tmp;
             if (s.find(tmp) != s.end()) {
                 s.erase(tmp);
             }
-        } else if (str == "toggle") {
+        }
+        else if (str == "toggle") {
             cin >> tmp;
             if (s.find(tmp) != s.end()) {
                 s.erase(tmp);
-            } else {
+            }
+            else {
                 s.insert(tmp);
             }
-        } else if (str == "all") {
+        }
+        else if (str == "all") {
             s = base;
-            // s = set<int>(v.begin(),v.end());
-        } else if(str == "empty") {
-            s.clear();
+            // s = set<int>(v.begin(), v.end());
+        }
+        else if (str == "empty") {
+            s = empty_set;
             // s.clear(); 
         }
     }
