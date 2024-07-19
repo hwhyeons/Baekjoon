088dff2f422cf62d9565474068ca384a126aa927
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2024-07-19 19:31:02 +0900

    [Gold IV] Title: N으로 만들기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/17255. N으로 만들기/N으로 만들기.py
+++ 백준/Gold/17255. N으로 만들기/N으로 만들기.py
@@ -1,26 +1,2 @@
-line = input()
-
-# d: dict[tuple[int,int],int] = dict()
-d: dict[str,int] = dict()
-
-def dfs(s: str):
-    if s in d:
-        return d[s]
-    if len(s) == 1:
-        return 1
-    left_remove_str = s[1:]
-    right_remove_str = s[:-1]
-    left_remove_answer = dfs(left_remove_str)
-    if left_remove_str == right_remove_str:
-        right_remove_answer = 0
-    else:
-        right_remove_answer = dfs(right_remove_str)
-    d[s] = left_remove_answer+right_remove_answer
-    return left_remove_answer+right_remove_answer
-
-
-
-
-
-answer = dfs(line)
-print(answer)
+def f(s):return f(l)+(f(r) if l!=(r:=s[:-1]) else 0) if (l:=s[1:]) else 1
+print(f(input()))
