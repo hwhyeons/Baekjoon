d7561b08790f777a71a50db0e3cf05ee16354332
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2024-09-27 19:31:35 +0900

    [Gold III] Title: 뒤집힌 계산기, Time: 584 ms, Memory: 33076 KB -BaekjoonHub

---
+++ 백준/Gold/20129. 뒤집힌 계산기/뒤집힌 계산기.py
@@ -0,0 +1,99 @@
+
+
+pl: list = list(map(int, input().split())) # ex) 1 2 3 4
+tmp = [("+",pl[0]),("-",pl[1]),("*",pl[2]),("/",pl[3])]
+abc = sorted(tmp,key= lambda k: k[1],reverse=True)
+oper1 = abc[0][0]
+oper2 = abc[1][0]
+oper3 = abc[2][0]
+oper4 = abc[3][0]
+line = input()
+idx = len(line)-1
+
+def skip_token() -> None:
+    global idx
+    idx-=1
+
+def get_token() -> str | None:
+    if idx == -1:
+        return None
+    return line[idx]
+
+def cal(e2: int, e1: int, op: str) -> int:
+    if op == "+":
+        return e2+e1
+    elif op == "-":
+        return e2-e1
+    elif op == "*":
+        return e2*e1
+    elif op == "/":
+        # C++
+        if (e2 < 0 < e1) or (e1 < 0 < e2):
+            return -(abs(e2)//abs(e1))
+        return e2//e1
+    else:
+        raise Exception("Improper Operator")
+
+def number() -> int:
+    global idx
+    if idx == -1:
+        raise Exception("err")
+    start_idx = idx
+    for i in range(start_idx,-1,-1):
+        if line[i].isnumeric():
+            idx-=1
+            continue
+        return int(line[i+1:start_idx+1])
+    return int(line[:start_idx+1])
+
+def op1() -> int:
+    num1 = number()
+    while True:
+        tok = get_token()
+        if tok == oper1:
+            skip_token()
+            num2 = number()
+            num1 = cal(num1,num2,oper1)
+            # num1 = num2 // num1
+        else:
+            return num1
+
+def op2() -> int:
+    exp1 = op1()
+    while True:
+        tok = get_token()
+        if tok == oper2:
+            skip_token()
+            exp2 = op1()
+            exp1 = cal(exp1,exp2,oper2)
+        else:
+            return exp1
+
+def op3() -> int:
+    exp1 = op2()
+    while True:
+        tok = get_token()
+        if tok == oper3:
+            skip_token()
+            exp2 = op2()
+            # exp1 = exp2 - exp1
+            exp1 = cal(exp1,exp2,oper3)
+        else:
+            return exp1
+
+def op4() -> int:
+    global oper4
+    exp1 = op3()
+    while True:
+        tok = get_token()
+        if not tok:
+            return exp1
+        if tok == oper4:
+            skip_token()
+            exp2 = op3()
+            exp1 = cal(exp1,exp2,oper4) # exp1 += exp2
+        else:
+            return exp1
+
+
+print(op4())
