bdeff39d9b0bfe09e30872b02f8b3b27f4b49483
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2024-10-16 15:26:40 +0900

    [Silver V] Title: 나이순 정렬, Time: 40 ms, Memory: 9840 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -1,45 +1,44 @@
 #include <iostream>
-#include <algorithm>
-#include <numeric>
-#include <string>
+#include <ranges>
 #include <vector>
+#include <array>
+#include <algorithm>
+#include <string>
+#include <queue>
 #include <set>
+#include <stack>
+#include <map>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
 
+typedef long long ll;
 using namespace std;
+// -----------------------------------------
 
-#define pair pair<int,string>
+class Person {
+public:
+    int age;
+    int idx;
+    string name;
+    auto operator<=>(const Person& rhs) const = default;
+};
 
-bool compare(pair p1, pair p2) {
-	if (p1.first < p2.first) {
-		return true;
-	} else {
-		return false;
-	}
-}
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	vector<pair> v;
-	int n;
-	cin >> n; 
-	for (int i=0;i<n; i++)
-	{
-		int t1;
-		string t2;
-		cin >> t1 >> t2;
-		v.push_back({t1,t2});
-	}
-	stable_sort(v.begin(),v.end(),compare);
-
-	auto it = v.begin();
-	while (it != v.end()) {
-		cout << (*it).first << " " << (*it).second << "\n";
-		it++;
-	}
-
-
-
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int N;
+    cin >> N;
+    vector<Person> v;
+    for (size_t i = 0; i < N; i++) {
+        int a;
+        string b;
+        cin >> a >> b;
+        v.push_back({ a,(int)i,b });
+    }
+    sort(v.begin(), v.end());
+    for (auto& i : v) {
+        cout << i.age << " " << i.name << "\n";
+    }
+    
 }
-	
-
-
