fec71be6e492a48a4e0800e89e41b9016fa505fe
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2024-11-05 18:19:56 +0900

    [Silver II] Title: N번째 큰 수, Time: 272 ms, Memory: 10812 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
@@ -1,40 +1,41 @@
-#define _CRT_SECURE_NO_WARNINGS
-
-#include <string>
 #include <iostream>
-#include <stack>
+#include <ranges>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <string>
 #include <queue>
-#include <climits>
+#include <numeric>
+#include <set>
+#include <stack>
+#include <map>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
 
+typedef long long ll;
 using namespace std;
+
+// -----------------------------------------
+// -----------------------------------------
+// -----------------------------------------
 
 
 int main() {
-	ios::sync_with_stdio(false); cin.tie(NULL);
-    int N,tmp;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int N;
     cin >> N;
-    int** arr = new int*[N];
-    int* lineIndex = new int[N];
-    fill_n(lineIndex, N, N - 1);
-    for (int i = 0; i < N; i++) {
-        arr[i] = new int[N];
-        for (int j = 0; j < N; j++) {
-            cin >> tmp;
-            arr[i][j] = tmp;
-        }
+    const int s = N * N;
+    vector<int> arr(s);
+    for (size_t i = 0; i < s; i++) {
+        cin >> arr[i];
     }
+    nth_element(arr.begin(), arr.begin() + N -1 , arr.begin() + s,std::greater<int>());
+    cout << arr.at(N-1);
 
-    int max = 0;
-    for (int i = 1; i <= N; i++) {
-        max = INT_MIN;
-        int maxIndex = -1;
-        for (int j = 0; j < N; j++) {
-            if (lineIndex[j] >= 0 && max < arr[lineIndex[j]][j]) {
-                max = arr[lineIndex[j]][j];
-                maxIndex = j;
-            }
-        }
-        lineIndex[maxIndex] -= 1;
-    }
-    cout << max;
+
+
+
+
 }
