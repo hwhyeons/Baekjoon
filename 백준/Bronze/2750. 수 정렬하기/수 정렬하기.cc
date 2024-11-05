54e9a620de012ceea0503669463eb4320beaff9a
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2024-11-05 15:23:37 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -1,25 +1,61 @@
-#define _CRT_SECURE_NO_WARNINGS
-
-#include <string>
 #include <iostream>
-#include <stack>
+#include <ranges>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <string>
+#include <queue>
 #include <set>
+#include <stack>
+#include <map>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
 
-
+typedef long long ll;
 using namespace std;
 
+// -----------------------------------------
+// -----------------------------------------
+// -----------------------------------------
+
+// 오름차순 퀵정렬
+void quickSort(vector<int>& v, const int left, const int right) {
+    if (left >= right) return;
+    const int pivotIndex = (left + right) / 2;
+    int l = left;
+    int r = right;
+    const int pivotValue = v.at(pivotIndex);
+    while (l <= r) {
+        while (v.at(l) < pivotValue) { // l이 인덱스 벗어날 일이 없는 이유 : pivotValue랑 같아지는 순간도 반복문 종료
+            l++;
+        }
+        while (v.at(r) > pivotValue) { // r이 인덱스 벗어날 일이 없는 이유 : pivotValue랑 같아지는 순간도 반복문 종료
+            r--;
+        }
+        if (l <= r) {
+            swap(v[l], v[r]);
+            // 교환 한 후에는 다시 값 변환 필요. 안그러면 예를들어 1 4 3 4 5 4 7에서 가장왼쪽4가 l, 중간 4가 pivot, 오른쪽 4가 r이면 무한루프
+            // 또한, l==r인상황에서 아래의 l++, r--를 안하면, 그 다음 바깥반복문 진행시에 위 두개의 while문에서 둘다 l,r값 변화가 없으므로 바깥 루프 무한반복
+            l++;
+            r--;
+        }
+    }
+    //quickSort(v, left, pivotIndex);
+    quickSort(v, left , r);
+    //quickSort(v, pivotIndex+1, right);
+    quickSort(v, l,right);
+}
 
 int main() {
-	ios::sync_with_stdio(false); cin.tie(NULL);
-	
-	set<int,less<int>> s;
-	int n, t;
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> t;
-		s.insert(t);
-	}
-	for (auto i = s.begin(); i != s.end(); i++) {
-		cout << *i << "\n";
-	}
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int N;
+    cin >> N;
+    vector<int> v(N);
+    for (size_t i = 0; i < N; i++)  cin >> v[i];
+    quickSort(v, 0, v.size() - 1);
+    for (auto& k : v) {
+        cout << k << "\n";
+    }
 }
