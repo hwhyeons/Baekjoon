3461eddd91477839848730e1828313ae5bc9bc31
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2024-11-11 14:22:40 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -1,3 +1,6 @@
-for _ in range(int(input())):
-    a=sum(v:=list(map(int,input().split()[1:])))/(k:=len(v))
-    print("{:.3f}%".format(sum(1 for i in v if i>a)/k*100))
+tcnt = int(input())
+for _ in range(tcnt):
+    v=list(map(int,input().split()[1:]))
+    avg=sum(v)/len(v)
+    cnt=sum(1 if i>avg else 0 for i in v)
+    print("{:.3f}%".format(cnt/len(v)*100))
