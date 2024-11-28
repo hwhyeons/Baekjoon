d66c3ebbde0c36e38fdab202d6a3fedf7eb02d29
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2024-11-28 14:19:48 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 328 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -1,28 +1,98 @@
 #include <iostream>
+#include <ranges>
+#include <vector>
+#include <array>
 #include <algorithm>
 #include <string>
-#include <vector>
-#include <deque>
-
+#include <queue>
+#include <numeric>
+#include <set>
+#include <stack>
+#include <map>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <bitset>
+typedef long long ll;
 using namespace std;
 
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	int n,t;
-	cin >> n;
-	int* arr = new int[n];
-	for (int i =0; i<n; i++) {
-		cin >> t;
-		arr[i] = t;
-	}
-	sort(arr,arr+n);
-	for (int i =0; i<n; i++) {
-		cout << arr[i] << "\n";
-	}
+// -----------------------------------------
+// -----------------------------------------
+// -----------------------------------------
+
+
+int MAX_VAL = 210000000;
+int len = 0;
+vector<int> v;
+
+void swapRecursiveUp(int childIdx) {
+    if (childIdx == 1) return;
+    int parentIdx = childIdx / 2;
+    int pVal = v[parentIdx];
+    int cVal = v[childIdx];
+    if (cVal < pVal) {
+        swap(v[parentIdx], v[childIdx]);
+        swapRecursiveUp(parentIdx);
+    }
+}
+void addHeap(int newVal) {
+    len++; // 3개 있으면 1,2,3번 인덱스에 들어있고 4번인덱스에 추가할 차례
+    v[len] = newVal;
+    swapRecursiveUp(len);
+}
+
+void swapRecursiveDown(int parentIndex) {
+    int leftChildIndex = parentIndex*2;
+    int rightChildIndex = leftChildIndex + 1;
+    int parentValue = v[parentIndex];
+    
+    // 자식이 없는 경우 -> 교환하지 않아야하니, 최소힙 상황에서는 MAX_VAL로 설정해서 swap 일어나지 않게
+    int lChildVal = leftChildIndex <= len ? v[leftChildIndex] : MAX_VAL;
+    int rChildVal = rightChildIndex <= len ? v[rightChildIndex] : MAX_VAL;
+    // 부모 노드가 가장 작은 경우 (최소힙)
+    if (parentValue < lChildVal && parentValue < rChildVal) {
+        return;
+    }
+
+    // 왼쪽 자식이 작은 경우
+    if (lChildVal < rChildVal) {
+        swap(v[leftChildIndex], v[parentIndex]);
+        swapRecursiveDown(leftChildIndex);
+    } else { // 오른쪽 자식이 작은 경우
+        swap(v[rightChildIndex], v[parentIndex]);
+        swapRecursiveDown(rightChildIndex);
+    }
+}
+
+int popHeap() {
+    int returnValue = v[1];
+    swap(v[len], v[1]); // 맨마지막 원소를 맨 앞으로
+    v[len] = 0; // 맨 마지막 원소 제거
+    len--;
+
+    // Down 방향으로 swap
+    swapRecursiveDown(1);
+    return returnValue;
 }
 
 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int N;
+    cin >> N;
+    v.assign(N+1, 0);
+    for (size_t i = 0; i < N; i++) {
+        int tmp; cin >> tmp;
+        addHeap(tmp);
+    }
+    
+    // test
+    for (size_t i = 0; i < N; i++) {
+        int val = popHeap();
+        cout << val << "\n";
+    }
 
 
 
+}
