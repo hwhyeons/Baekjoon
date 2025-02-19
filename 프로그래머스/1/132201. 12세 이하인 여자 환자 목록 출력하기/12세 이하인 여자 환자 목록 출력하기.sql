dc0b8160bcae5bca7d08482bb43c78f0f8c967fd
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2025-02-19 18:54:55 +0900

    [level 1] Title: 12세 이하인 여자 환자 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/132201. 12세 이하인 여자 환자 목록 출력하기/12세 이하인 여자 환자 목록 출력하기.sql
+++ 프로그래머스/1/132201. 12세 이하인 여자 환자 목록 출력하기/12세 이하인 여자 환자 목록 출력하기.sql
@@ -1,12 +1,17 @@
 -- 코드를 입력하세요
 
-select PT_NAME, PT_NO, GEND_CD, AGE, TLNO from 
+select PT_NAME, PT_NO, GEND_CD, AGE, 
+CASE WHEN TLNO is not NULL then TLNO ELSE 'NONE' end
 
-((SELECT PT_NAME, PT_NO, GEND_CD, AGE, TLNO 
-from PATIENT where TLNO is not NULL) 
-UNION
-(select PT_NAME, PT_NO, GEND_CD, AGE, 'NONE' as TLNO 
-from PATIENT where TLNO is NULL)) as T
-
+from PATIENT 
 where AGE <= 12 and GEND_CD = 'W' 
 ORDER BY AGE desc, pt_name asc
+
+# ((SELECT PT_NAME, PT_NO, GEND_CD, AGE, TLNO 
+# from PATIENT where TLNO is not NULL) 
+# UNION
+# (select PT_NAME, PT_NO, GEND_CD, AGE, 'NONE' as TLNO 
+# from PATIENT where TLNO is NULL)) as T
+
+# where AGE <= 12 and GEND_CD = 'W' 
+# ORDER BY AGE desc, pt_name asc
