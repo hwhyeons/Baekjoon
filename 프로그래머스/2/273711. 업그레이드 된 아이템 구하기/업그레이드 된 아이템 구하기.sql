78e81529b234c13032674910676542f8cfb43da9
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2025-04-17 15:05:12 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -0,0 +1,21 @@
+# 1. RARE인 아이템만 ITEM_INFO 추출
+#   -> ITEM_INFO_RARE
+with ITEM_INFO_RARE as (
+select * from ITEM_INFO where RARITY = "RARE"
+),
+# 2. 이제 이 레어 아이템의 다음 강화 템들을 추출
+#   => ITEM_TREE에서 parent_item_id가 ITEM_INFO_RARE.ITEM_ID에 들어 있는 대상의 
+#   ITEM_ID 추출 ->  t3_tuple
+t3_tuple as (
+select ITEM_ID
+    from ITEM_TREE 
+    where ITEM_TREE.PARENT_ITEM_ID in (select ITEM_ID from ITEM_INFO_RARE)
+)
+
+
+# 3. 다시 맨처음 ITEM_INFO 에서 "ITEM_ID in t3_tuple"을
+# 만족하는 것들 뽑아서 정렬후 리턴
+select ITEM_ID,ITEM_NAME,RARITY
+from ITEM_INFO
+where ITEM_ID in (select * from t3_tuple)
+order by ITEM_ID desc
